6dba45e2f4feb68cf18a3c04d618e6deefa79c34
thanhtung3496 <33385100+thanhtung3496@users.noreply.github.com>
2019-11-17 11:55:13 -0500

    created switch for edit_post

---
+++ admin/includes/edit_post.php
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+
+<form action="" method="post" enctype="multipart/form-data">    
+
+
+    <div class="form-group">
+        <label for="title">Post Title</label>
+        <input type="text" class="form-control" name="title">
+    </div>
+
+    <div class="form-group">
+        <label for="post_category">Post Category Id</label>
+        <input type="text" class="form-control" name="post_category_id">
+    </div>
+
+    <div class="form-group">
+        <label for="title">Post Author</label>
+        <input type="text" class="form-control" name="author">
+    </div>
+    
+    <div class="form-group">
+        <select name="post_status" id="">
+            <option value="draft">Post Status</option>
+            <option value="published">Published</option>
+            <option value="draft">Draft</option>
+        </select>
+    </div>
+    
+    <div class="form-group">
+        <label for="post_image">Post Image</label>
+        <input type="file" name="image">
+    </div>
+    
+    <div class="form-group">
+        <label for="post_tags">Post Tags</label>
+        <input type="text" class="form-control" name="post_tags">
+    </div>
+
+    
+    <div class="form-group">
+        <label for="post_content">Post Content</label>
+        <textarea class="form-control"name="post_content" id="body" cols="30" rows="10">
+        </textarea>
+    </div>
+    
+    
+    <!--<div class="form-group">
+    <label for="users">Users</label>
+    <select name="post_user" id="">
+
+
+
+
+    </select>
+
+    </div>-->
+
+    
+    <div class="form-group">
+        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+    </div>
+</form>
+
+
